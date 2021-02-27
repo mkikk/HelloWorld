5358ae6377e98a4bec2cc5e7a8f1be4dd28eaf85
ragnarkadai <ragnar.kadai@gmail.com>
2021-02-27 20:28:20 +0200

    Muudatused

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("goodbye world");
+        System.out.println("ô¿ô");
+        System.out.println("Muudatused on tehtud");
 
     }
 
